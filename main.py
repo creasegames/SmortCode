4b15bcdd60c0db06216468a9e0ff1488f058aefb
slowpoke111 <110692534+slowpoke111@users.noreply.github.com>
2022-08-05 22:32:43 -0400

    Simplified the file system

--- main.py
+++ main.py
@@ -5,25 +5,28 @@ def namer(name):
   name=lines[i]
   name = name[0:len(name)-1]
   return name
-lines=[file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline(),file.readline()]
-print(lines)
+lines=[file.readlines()]
+
+for i in lines:
+  i=i.strip()
+
 for i in range(10):
   if lines[i]=='!say\n':
     i+=1
     print(lines[i])
-  if lines[i]=='!read\n':
+  if lines[i]=='!read':
     i+=1
     text = open(namer(lines[i]),'r')
     print(text.read())
     text.close()
-  if lines[i]=='!edit\n':
+  if lines[i]=='!edit':
     i+=1
     text = open(namer(lines[i]),'w')
     i+=1
     text.write(lines[i])
     text.close()
-  if lines[i]=='!clear\n':
+  if lines[i]=='!clear':
     os.system('clear')
-  if lines[i]=='!pause\n':
+  if lines[i]=='!pause':
     i+=1
     time.sleep(float(lines[i]))
